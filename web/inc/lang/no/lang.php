41ce78a47f8a208ff27d86b43e0a710d15983e42
gianluigi <gianluigi.calcaterra@gmail.com>
2006-12-30 01:36:54 +0100

    Added dokuwiki for website

---
+++ web/inc/lang/no/lang.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Norwegian language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Reidar Mosvold <Reidar.Mosvold@hit.no>
+ * @author     Jorge Barrera Grandon <jorge@digitalwolves.org>
+ */
+$lang['encoding']   = 'utf-8';
+$lang['direction']  = 'ltr';
+ 
+$lang['btn_edit']   = 'Rediger denne siden';
+$lang['btn_source'] = 'Vis kildetekst';
+$lang['btn_show']   = 'Vis siden';
+$lang['btn_create'] = 'Lag denne siden';
+$lang['btn_search'] = 'Søk';
+$lang['btn_save']   = 'Lagre';
+$lang['btn_preview']= 'Forhåndsvis';
+$lang['btn_top']    = 'Til toppen av siden';
+$lang['btn_newer']  = '<< nyere';
+$lang['btn_older']  = 'eldre >>';
+$lang['btn_revs']   = 'Historikk';
+$lang['btn_recent'] = 'Siste endringer';
+$lang['btn_upload'] = 'Last opp';
+$lang['btn_cancel'] = 'Avbryt';
+$lang['btn_index']  = 'Indeks';
+$lang['btn_secedit']= 'Rediger';
+$lang['btn_login']  = 'Logg inn';
+$lang['btn_logout'] = 'Logg ut';
+$lang['btn_admin']  = 'Admin';
+$lang['btn_update'] = 'Oppdater';
+$lang['btn_delete'] = 'Slett';
+$lang['btn_back']   = 'Tilbake';
+$lang['btn_backlink']    = "Tilbakelinker";
+$lang['btn_backtomedia'] = 'Tilbake til valg av mediafil';
+$lang['btn_subscribe']   = 'Abonner på endringer';
+$lang['btn_unsubscribe'] = 'Ikke abonner på endringer';
+ 
+$lang['loggedinas'] = 'Logg inn som';
+$lang['user']       = 'Brukernavn';
+$lang['pass']       = 'Passord';
+$lang['passchk']    = 'Bekreft passord';
+$lang['remember']   = 'Husk på meg';
+$lang['fullname']   = 'Fulle navn';
+$lang['email']      = 'E-Post';
+$lang['register']   = 'Registrer deg';
+$lang['badlogin']   = 'Ugyldig brukernavn og/eller passord.';
+ 
+$lang['regmissing'] = 'Vennligst fyll ut alle felt.';
+$lang['reguexists'] = 'Det finnes allerede en konto med dette brukernavnet.';
+$lang['regsuccess'] = 'Brukerkonto har blitt laget og passord har blitt sendt via e-post.';
+$lang['regsuccess2']= 'Brukeren har blitt laget.';
+$lang['regmailfail']= 'En feil oppstod da passordet ditt skulle sendes via e-post. Vennligst kontakt administratoren!';
+$lang['regbadmail'] = 'Den angitte e-post adressen ser ut til å være ugyldig. Vennligst kontakt administratoren om du anser dette som feilaktig.';
+$lang['regbadpass'] = 'De to angitte passordene er ikke like, vennligst forsøk igjen.';
+$lang['regpwmail']  = 'Ditt DokuWiki passord';
+$lang['reghere']    = 'Har du ikke en konto enda? Lag deg en';
+ 
+$lang['txt_upload']   = 'Velg fil som skal lastes opp';
+$lang['txt_filename'] = 'Skriv inn wikinavn (alternativt)';
+$lang['txt_overwrt']  = 'Overskriv eksisterende fil';
+$lang['lockedby']     = 'Stengt av';
+$lang['lockexpire']   = 'Avstengningen opphører';
+$lang['willexpire']   = 'Din redigeringslås for dette dokumentet kommer snart til å opphøre.\nFor å unngå versjonskonflikter bør du forhåndsvise dokumentet ditt for å forlenge redigeringslåsen.';
+ 
+$lang['notsavedyet'] = 'Ulagrede endringer vil gå tapt.\nVil du fortsette?';
+ 
+$lang['rssfailed']   = 'En feil oppstod da denne kilden skulle hentes:';
+$lang['nothingfound']= 'Ingen data funnet.';
+ 
+$lang['mediaselect'] = 'Valg av mediafil';
+$lang['fileupload']  = 'Mediafil Opplasting';
+$lang['uploadsucc']  = 'Opplastingen var vellykket';
+$lang['uploadfail']  = 'Opplastingen var mislykket. Gale rettigheter?';
+$lang['uploadwrong'] = 'Opplastingen ble nektet. Denne filendelsen er ikke tillatt!';
+$lang['uploadexist'] = 'Filen eksisterer. Ingenting har blitt gjort.';
+$lang['deletesucc']  = 'Filen "%s" har blitt slettet.';
+$lang['deletefail']  = '"%s" kunne ikke slettes - sjekk rettighetene.';
+$lang['mediainuse']  = 'Filen "%s" har ikke biltt slettet - den er fortsatt i bruk.';
+$lang['namespaces']  = 'sidenavn';
+$lang['mediafiles']  = 'Tilgjengelige filer i';
+ 
+$lang['reference']   = 'Referanser for';
+$lang['ref_inuse']   = 'Denne filen kan ikke slettes fordi den er fortsatt i bruk av følgende sider:';
+$lang['ref_hidden']  = 'Noen referanser er på sider du ikke har tilgang til å lese';
+ 
+$lang['hits']       = 'Treff';
+$lang['quickhits']  = 'Matchende wikinavn';
+$lang['toc']        = 'Innholdsfortegnelse';
+$lang['current']    = 'nåværende versjon';
+$lang['yours']      = 'Din versjon';
+$lang['diff']       = 'Vis forskjeller mot nåværende versjon';
+$lang['line']       = 'Linje';
+$lang['breadcrumb'] = 'Spor';
+$lang['lastmod']    = 'Sist modifisert';
+$lang['by']         = 'av';
+$lang['deleted']    = 'fjernet';
+$lang['created']    = 'opprettet';
+$lang['restored']   = 'gjenopprettet til en tidligere versjon';
+$lang['summary']    = 'Redigeringskommentar';
+ 
+$lang['mail_newpage'] = 'side lagt til:';
+$lang['mail_changed'] = 'side endret:';
+ 
+$lang['nosmblinks'] = 'Lenker til Windowsressurser fungerer bare i Microsoft sin Internet Explorer.\nDu kan fortsatt kopiere og lime inn lenken.';
+ 
+$lang['qb_alert']   = 'Vennligst skriv inn teksten du ønsker å formatere.\nDen vil bli lagt til mot slutten av dokumentet.';
+$lang['qb_bold']    = 'Fet tekst';
+$lang['qb_italic']  = 'Kursiv tekst';
+$lang['qb_underl']  = 'Understreket tekst';
+$lang['qb_code']    = 'Kodetekst';
+$lang['qb_strike']  = 'Gjennomstreket tekst';
+$lang['qb_h1']      = 'Nivå 1 Rubrikk';
+$lang['qb_h2']      = 'Nivå 2 Rubrikk';
+$lang['qb_h3']      = 'Nivå 3 Rubrikk';
+$lang['qb_h4']      = 'Nivå 4 Rubrikk';
+$lang['qb_h5']      = 'Nivå 5 Rubrikk';
+$lang['qb_link']    = 'Intern Lenke';
+$lang['qb_extlink'] = 'Ekstern Lenke';
+$lang['qb_hr']      = 'Horisontal strek';
+$lang['qb_ol']      = 'Punkt for sortert punktliste';
+$lang['qb_ul']      = 'Punkt for usortert punktliste';
+$lang['qb_media']   = 'Legg til bilder og andre filer';
+$lang['qb_sig']     = 'Føy til signatur';
+ 
+$lang['del_confirm']= 'Slett denne oppføringen?';
+$lang['admin_register']= 'Legg til ny bruker';
+
+$lang['spell_start'] = 'Sjekk stavelse';
+$lang['spell_stop']  = 'Tilbake til redigering';
+$lang['spell_wait']  = 'Vennligst vent...';
+$lang['spell_noerr'] = 'Ingen feil funnet';
+$lang['spell_nosug'] = 'Ingen forslag';
+$lang['spell_change']= 'Endre';
+ 
+$lang['metaedit']    = 'Rediger Metadata';
+$lang['metasaveerr'] = 'Skriving av metadata feilet';
+$lang['metasaveok']  = 'Metadata lagret';
+$lang['img_backto']  = 'Tilbake til';
+$lang['img_title']   = 'Tittel';
+$lang['img_caption'] = 'Billedtekst';
+$lang['img_date']    = 'Dato';
+$lang['img_fname']   = 'Filnavn';
+$lang['img_fsize']   = 'Størrelse';
+$lang['img_artist']  = 'Fotograf';
+$lang['img_copyr']   = 'Copyright';
+$lang['img_format']  = 'Format';
+$lang['img_camera']  = 'Kamera';
+$lang['img_keywords']= 'Nøkkelord';
+ 
+$lang['subscribe_success']  = 'La til %s som abonnent av listen for %s';
+$lang['subscribe_error']    = 'En feil oppstod når %s skulle legges til som abonnent av listen for %s';
+$lang['subscribe_noaddress']= 'Det er ingen adresse som er assosiert med din login. Du kan dermed ikke bli lagt til som abonnent av listen.';
+$lang['unsubscribe_success']= 'Fjernet %s som abonnent for listen %s';
+$lang['unsubscribe_error']  = 'En feil oppstod når %s skulle fjernes som abonnent fra listen for %s';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
